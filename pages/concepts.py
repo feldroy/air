99eb95e73e0c5c3a7ac04a9a25d737c9ed3cd22f
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-07-29 10:28:53 +0800

    Implement air pages
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

---
+++ pages/concepts.py
@@ -0,0 +1,11 @@
+import air
+
+def render(request: air.Request):
+    return air.Children(
+        air.Title('Concepts'),
+        air.Article(
+            air.H1('Concepts'),
+            air.P('Why and how Air works.'),
+            class_='prose'
+        )
+    )
