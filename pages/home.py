3ba9e46f7e062900a1f60ecba53e2f0f16a120ae
Audrey M. Roy Greenfeld <audrey@feldroy.com>
2025-08-01 20:53:53 +0800

    Add Air 3-color svg

--- pages/home.py
+++ pages/home.py
@@ -3,6 +3,6 @@ import air
 
 def render(request: air.Request):
     return air.Children(
-        air.H1("Air"),
+        air.Img(src="https://air-svgs.fastapicloud.dev/static/air-3color.svg", width="300", alt="Air logo"),
         air.H2("The new Python web framework built on FastAPI.")
     )
