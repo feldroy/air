9e6711c1ca34ed1bc0cc5b13956cc1e11ba37612
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-05 08:57:33 +0800

    Tweak links

--- pages/concepts.py
+++ pages/concepts.py
@@ -6,14 +6,14 @@ from airdocs.utils import make_link_from_mdpath
 
 
 def render(request: air.Request):
-    files = Path("pages/concepts").glob("*.md")
-    links = [air.Li(make_link_from_mdpath(x)) for x in files]
+    # files = Path("pages/concepts").glob("*.md")
+    # links = [air.Li(make_link_from_mdpath(x)) for x in files]
     return air.Children(
         air.Title("Concepts"),
         air.Article(
             air.H1("Concepts"),
             air.P("Why and how Air works."),
-            air.Ul(*links),
+            # air.Ul(*links),
             class_="prose",
         ),
     )
