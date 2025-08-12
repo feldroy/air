c7fbad254eb9a2777e3557ca52a1971f55278952
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-13 06:45:25 +0800

    Support MVP.css headers not being in body

--- tests/test_layouts.py
+++ tests/test_layouts.py
@@ -23,3 +23,16 @@ def test_mvpcss_layout():
         """<!doctype html><html><head><link href="https://unpkg.com/mvp.css" rel="stylesheet" /><script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js" integrity="sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm" crossorigin="anonymous"></script><title>Cheese Monger</title></head><body><main><h1>Cheese Monger</h1></main></body></html>"""
         == html
     )
+
+
+def test_mvpcss_layout_header():
+    html = air.layouts.mvpcss(
+        air.Header(
+            air.H1("This is in the header"),
+        ),
+        air.P("This is in the main"),
+    )
+    assert (
+        html
+        == '<!doctype html><html><head><link href="https://unpkg.com/mvp.css" rel="stylesheet" /><script src="https://cdn.jsdelivr.net/npm/htmx.org@2.0.6/dist/htmx.min.js" integrity="sha384-Akqfrbj/HpNVo8k11SXBb6TlBWmXXlYQrCSqEWmyKJe+hDm3Z/B2WVG4smwBkRVm" crossorigin="anonymous"></script></head><body><header><h1>This is in the header</h1></header><main><p>This is in the main</p></main></body></html>'
+    )
