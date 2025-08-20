b75afa1006c859473e34392b7cf543a4582f5863
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-08-20 19:20:55 +0800

    Docstring improvements

--- src/air/background.py
+++ src/air/background.py
@@ -11,28 +11,26 @@ class BackgroundTasks(FastAPIBackgroundTasks):
     A collection of background tasks that will be called after a response has been
     sent to the client.
 
-    ## Example
+    Example:
 
-    ```python
-    from air import BackgroundTasks, Air
-    from air.layouts import picocss
+        from air import BackgroundTasks, Air
 
-    app = Air()
+        app = Air()
 
 
-    def write_notification(email: str, message=""):
-        with open("log.txt", mode="w") as email_file:
+        def write_notification(email: str, message=""):
+            with open("log.txt", mode="w") as email_file:
+                content = f"notification for {email}: {message}"
+                email_file.write(content)
+
+
+        @app.post("/send-notification/{email}")
+        async def send_notification(email: str, background_tasks: BackgroundTasks):
+            message = "some notification"
+            background_tasks.add_task(write_notification, email, message=message)
             content = f"notification for {email}: {message}"
-            email_file.write(content)
+            return air.layouts.picocss(air.Title(content), air.H1(content))
 
-
-    @app.post("/send-notification/{email}")
-    async def send_notification(email: str, background_tasks: BackgroundTasks):
-        message = "some notification"
-        background_tasks.add_task(write_notification, email, message=message)
-        content = f"notification for {email}: {message}"
-        return picocss(air.Title(content), air.H1(content))
-    ```
     """
 
     def add_task(
