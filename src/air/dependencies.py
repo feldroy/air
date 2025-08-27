0ed18ec067742d4c0d943a7afae0fcc6d3083ada
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2025-08-27 14:36:34 +0800

    Implement hx boost in layout functions (#361)
    * Implement hx boost in layout functions
    
    * Tests for new layout feature

--- src/air/dependencies.py
+++ src/air/dependencies.py
@@ -1,9 +1,9 @@
 """Tools for handling dependecies, for things like handling incoming data from client libraries like HTMX."""
 
-from fastapi import Header
+from fastapi import Depends, Header
 
 
-def is_htmx_request(hx_request: str = Header(default=None)) -> bool:
+def _is_htmx_request(hx_request: str = Header(default=None)) -> bool:
     """Dependency injection function used to determine if request is coming from HTMX. Checks if 'hx-request' header is in the HTTP request.
 
     Returns:
@@ -12,6 +12,7 @@ def is_htmx_request(hx_request: str = Header(default=None)) -> bool:
     Example:
 
         import air
+        from fastapi import Depends
 
         app = air.App()
 
@@ -20,3 +21,6 @@ def is_htmx_request(hx_request: str = Header(default=None)) -> bool:
             return air.H1(f"Is HTMX request: {is_htmx}")
     """
     return hx_request is not None and hx_request.lower() == "true"
+
+
+is_htmx_request = Depends(_is_htmx_request)
