93294dbad2c6bf23ded4e6fd80a7b72fdcb16fc3
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2025-08-27 09:12:22 +0800

    Create examples directory (#360)
    * Create examples directory
    
    * Add pyinstrument as a dependency

---
+++ examples/instrument.py
@@ -0,0 +1,29 @@
+"""
+This project requires the "pyinstrument" package, intentionally not
+included in the dependencies of the Air project.
+
+Call pyinstrument by adding '?profile=1' after any URL
+"""
+
+from pyinstrument import Profiler
+
+import air
+
+app = air.Air()
+
+
+@app.middleware("http")
+async def profile_request(request: air.Request, call_next):
+    profiling = request.query_params.get("profile", False)
+    if profiling:
+        profiler = Profiler()
+        profiler.start()
+        await call_next(request)
+        profiler.stop()
+        return air.responses.HTMLResponse(profiler.output_html())
+    return await call_next(request)
+
+
+@app.page
+def index():
+    return air.layouts.mvpcss(air.H1("Home"), air.Ol(*[air.Li(x) for x in range(1, 100)]))
