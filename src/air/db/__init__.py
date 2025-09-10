8c656a35e3cc53a47d45f516255c3082b89aaa3f
Daniel Roy Greenfeld <pydanny@users.noreply.github.com>
2025-09-10 19:16:01 +0800

    FEAT: Common sense async/sync defaults for SQLModel and SQLAlchemy (#399)
    * Add basic database features
    
    * Non-explosive imports
    
    * Ungainly unworkable
    
    * Bring in proven db session mechanisms
    
    * Improving test coverage
    
    * Improving test coverage
    
    * Get new db stuff working
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>
    
    * Get test coverage to 100%
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>
    
    * Use explicit imports for SQL and fix async url cleanup
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>
    
    * Final QA cleanup
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>
    
    * Add docstrings to sql module
    
    Co-Authored-By: Audrey M. Roy Greenfeld <audrey@feldroy.com>
    
    * cleanup
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>
    
    * Improve docs
    
    ---------
    
    Co-authored-by: Audrey Roy Greenfeld <audrey@feldroy.com>

---
+++ src/air/db/__init__.py
@@ -0,0 +1,23 @@
+try:
+    from .sql import (
+        AsyncSession as AsyncSession,
+        async_session_dependency as async_session_dependency,
+        create_async_engine as create_async_engine,
+        create_async_session as create_async_session,
+        create_sync_engine as create_sync_engine,
+        get_async_session as get_async_session,
+    )
+except ImportError:  # pragma: no cover
+    msg = "air.db.sql requires installing the sqlmodel and greenlet packages."
+
+    class NotImportable:
+        def __getattribute__(self, name):
+            raise RuntimeError(msg)
+
+        def __str__(self):
+            return msg
+
+        def __repr__(self):
+            return msg
+
+    sql = NotImportable()
