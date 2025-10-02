ad085c115e107d240ec0f28c7bfd7f0c7cfd0f68
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-10-02 15:57:16 +0800

    Implement AirRoute customer route manager

--- src/air/requests.py
+++ src/air/requests.py
@@ -5,5 +5,5 @@ from starlette.requests import Request as _Request
 
 class Request(_Request):
     @property
-    def htmx(self):
-        return self.headers["hx_request"] is not None and self.headers["hx_request"].lower() == "true"
+    def htmx(self) -> bool:
+        return self.headers.get("hx-request", "").lower() == "true"
