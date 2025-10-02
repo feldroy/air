a1335289a15c89d4771069f76f19dc34973f51f0
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-10-02 14:26:43 +0800

    Implement .htmx method on air.Request

--- src/air/requests.py
+++ src/air/requests.py
@@ -1,3 +1,9 @@
 """Tools for handling requests"""
 
-from starlette.requests import Request as Request
+from starlette.requests import Request as _Request
+
+
+class Request(_Request):
+    @property
+    def htmx(self):
+        return self.headers["hx_request"] is not None and self.headers["hx_request"].lower() == "true"
