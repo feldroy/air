922e22c19dccf60c9fae78b8f168564bbdef9960
Daniel Roy Greenfeld <daniel@feldroy.com>
2025-10-25 13:26:19 +0800

    Trial version of src_example

---
+++ src_examples/applications__Air__page__test.py
@@ -0,0 +1,21 @@
+from applications__Air__page import app
+from fastapi.testclient import TestClient
+
+
+def test_page_routes() -> None:
+    client = TestClient(app)
+
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert response.text == "<h1>I am the home page</h1>"
+
+    response = client.get("/data")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert response.text == "<h1>I am the data page</h1>"
+
+    response = client.get("/about-us")
+    assert response.status_code == 200
+    assert response.headers["content-type"] == "text/html; charset=utf-8"
+    assert response.text == "<h1>I am the about page</h1>"
