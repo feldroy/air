610824b702573f3828c5b628ffb663d1ac3111cb
Edchel Stephen Nini <edchelstephens@gmail.com>
2025-10-26 13:31:24 +0800

    chore: test_parse_filename_class_returns_None_on_non_python_files.

--- tests/scripts/test_copy_src_example_to_callable.py
+++ tests/scripts/test_copy_src_example_to_callable.py
@@ -17,5 +17,17 @@ def test_parse_filename_class_returns_None_on_filename_ends_with___init_py() -> 
     assert parse_filename_class("__init__.py") is None
 
 
-
-
+@pytest.mark.current
+@pytest.mark.parametrize(
+    "filename",
+    [
+        "getCallable.js",
+        "example.txt",
+        "data.json",
+        "script.sh",
+        "README.md",
+    ],
+)
+def test_parse_filename_class_returns_None_on_non_python_files(filename: str) -> None:
+    """parse_filename_class() returns None when filename is not a python file name."""
+    assert parse_filename_class(filename) is None
