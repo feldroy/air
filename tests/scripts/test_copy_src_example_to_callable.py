3807bcba60706c4e7aad7e1297f8e4a665715f21
Edchel Stephen Nini <edchelstephens@gmail.com>
2025-10-26 14:00:03 +0800

    chore: test_parse_filename_class_returns_a_tuple_of_module_class_and_method.

--- tests/scripts/test_copy_src_example_to_callable.py
+++ tests/scripts/test_copy_src_example_to_callable.py
@@ -31,3 +31,20 @@ def test_parse_filename_class_returns_None_on_filename_ends_with___init_py() -> 
 def test_parse_filename_class_returns_None_on_non_python_files(filename: str) -> None:
     """parse_filename_class() returns None when filename is not a python file name."""
     assert parse_filename_class(filename) is None
+
+
+@pytest.mark.current
+@pytest.mark.parametrize(
+    "filename, expected_output",
+    [
+        ("applications__Air__page.py", ("applications", "Air", "page")),
+        ("applications__Air__get.py", ("applications", "Air", "get")),
+        ("applications__Air__post.py", ("applications", "Air", "post")),
+    ],
+)
+def test_parse_filename_class_returns_a_tuple_of_module_class_and_method(
+    filename: str, expected_output: tuple[str, str, str]
+) -> None:
+    """parse_filename_class() returns a tuple of (module, class,  method) for a valid class method on a module."""
+
+    assert parse_filename_class(filename) == expected_output
