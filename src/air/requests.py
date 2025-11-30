7cc540ee47397ae492fbd51b39946280b8b21d0f
sankarebarri <sankarebarri_dev@yahoo.com>
2025-11-30 17:33:22 +0000

    chore: enable Ruff FIX rule and clean TODO in the comments

--- src/air/requests.py
+++ src/air/requests.py
@@ -78,7 +78,7 @@ class HtmxDetails:
     trigger_name: str | None = field(init=False)
     """The name of the triggered element if it exists, or `None`. Based on the `HX-Trigger-Name` header."""
 
-    # TODO this requires an HTMX extension, evaluate if it makes sense to use it
+    # This requires an HTMX extension, its usefulness may need further evaluation.
     triggering_event: Any = field(init=False)
 
     def __post_init__(self) -> None:
